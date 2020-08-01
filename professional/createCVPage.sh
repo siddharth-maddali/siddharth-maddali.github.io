763f0504158dba7f906b65cff880a778db0fa2e6
Siddharth Maddali <siddharth.mv@protonmail.com>
2020-08-01 04:15:08 -0500

    Added phys rev Appl, added GScholar logo next to Publications header in CV

--- professional/createCVPage.sh
+++ professional/createCVPage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp CV-original.ipynb CV.ipynb
-sed -i "s/# References/# Publications/g" CV.ipynb
+sed -i "s/# References/# Publications \<a href=\\\\\"https:\/\/scholar\.google\.com\/citations?user=hsYqvQIAAAAJ\&hl=en\&oi=ao\\\\\"\>\<i class=\\\\\"ai ai-google-scholar-square ai\\\\\"\>\<\/i\>\<\/a\>/g" CV.ipynb
 sed -i "/cite{/d" CV.ipynb # removes explicit citations
 jupyter nbconvert --to markdown CV.ipynb
 cat header.txt CV.md > cv.md
@@ -9,8 +9,8 @@ sed -i "s/Maddali, S\./**Maddali, S\.**/g" cv.md
 sed -i "s/Maddali S\./**Maddali S\.**/g" cv.md
 sed -i "s/Maddali Siddharth/**Maddali Siddharth**/g" cv.md
 sed -i "s/S\. Maddali/**S\. Maddali**/g" cv.md
-sed -i "s/\`\`_/ _/g" cv.md		# cleaning up Jupyter latex_env's
-sed -i "s/_'',/_ ,/g" cv.md		# horrible citation formatting
+#sed -i "s/\`\`_/ _/g" cv.md		# cleaning up Jupyter latex_env's
+#sed -i "s/_'',/_ ,/g" cv.md		# horrible citation formatting
 
 # create corresponding page used for PDF dump
 cp cv.md cv_download.md
